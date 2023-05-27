3965c82862e90f82e3479cf3363195daa57873a5
Alfredo Lorentiar Santonanda <alorentiar03@gmail.com>
2023-05-27 20:49:33 +0700

    success

--- VideoScreen.tsx
+++ VideoScreen.tsx
@@ -1,69 +1,63 @@
-import React, { useRef, useState } from 'react';
-import { View, Button, StyleSheet, Dimensions } from 'react-native';
-import Video from 'react-native-video';
+import React, { useRef, useState } from "react";
+import { View, Button, Text } from "react-native";
+import Video from "react-native-video";
 
-const VideoScreen = () => {
-  const videoRef = useRef(null);
-  const [videoIndex, setVideoIndex] = useState(0);
-  const [currentTime, setCurrentTime] = useState(0);
+const VideoScreen: React.FC = () => {
+  const videoRef = useRef<Video>(null);
+  const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
   const [seekTime, setSeekTime] = useState(0);
+  const [videoLoaded, setVideoLoaded] = useState(false);
   const [paused, setPaused] = useState(false);
-  const videos = [
-    {
-      uri: 'https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8',
-    },
-    {
-      uri: 'https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8',
-    },
+
+  const videoUrls = [
+    "https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8",
+    "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8",
   ];
 
   const switchVideo = () => {
     if (videoRef.current) {
-      setPaused(true);
-      setSeekTime(currentTime);
-      setVideoIndex(prevIndex => (prevIndex === 0 ? 1 : 0));
+      setPaused(true); // Pause the current video
+      videoRef.current.seek(seekTime); // Seek to the saved seek time
+      setCurrentVideoIndex((prevIndex) => (prevIndex + 1) % videoUrls.length);
+      setVideoLoaded(false);
     }
   };
 
-  const onProgress = data => {
-    setCurrentTime(data.currentTime);
+  const onProgress = (data: any) => {
+    if (!videoLoaded) {
+      setVideoLoaded(true); // Mark video as loaded once the first progress update is received
+    }
+
+    setSeekTime(Math.floor(data.currentTime)); // Update the seek time
   };
 
-  const onPlaybackResume = () => {
-    setSeekTime(currentTime);
+  const onLoad = () => {
+    if (!videoLoaded) {
+      setVideoLoaded(true); // Mark video as loaded when it finishes loading
+      setPaused(false); // Start playing the video
+    }
   };
 
   return (
-    <View style={styles.container}>
+    <View style={{ flex: 1 }}>
+      <Text style={{ fontSize: 16, fontWeight: "bold", textAlign: "center", marginTop: 20 }}>
+        Seek Time: {seekTime} seconds
+      </Text>
+
       <Video
         ref={videoRef}
-        source={videos[videoIndex]}
-        style={styles.video}
-        onProgress={onProgress}
-        onPlaybackResume={onPlaybackResume}
+        source={{ uri: videoUrls[currentVideoIndex] }}
+        style={{ flex: 1 }}
         resizeMode="contain"
-        controls
-        paused={paused}
+        controls={true}
+        paused={paused} // Control the playback state
+        onProgress={onProgress}
+        onLoad={onLoad}
       />
-      <Button
-        title="Switch Video"
-        onPress={switchVideo}
-        disabled={!videoRef.current}
-      />
+
+      <Button title="Switch Video" onPress={switchVideo} />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  video: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').width * (9 / 16), // Assuming 16:9 aspect ratio
-  },
-});
 
 export default VideoScreen;
