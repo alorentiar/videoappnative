6ab8498877f117c99b8257f26b3b8b002c7ea6ef
Alfredo Lorentiar Santonanda <alorentiar03@gmail.com>
2023-05-28 02:59:05 +0700

    update

--- VideoScreen.tsx
+++ VideoScreen.tsx
@@ -2,20 +2,29 @@ import React, {useRef, useState, useEffect} from 'react';
 import {View, Button, Text} from 'react-native';
 import Video, {OnProgressData} from 'react-native-video';
 
+interface VideoInfo {
+  url: string;
+  seekTime: number;
+}
+
 const VideoScreen: React.FC = () => {
   const videoRef = useRef<Video | null>(null);
   const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
   const [videoLoaded, setVideoLoaded] = useState(false);
-  const [totalSeekTime, setTotalSeekTime] = useState(0);
-
-  const videoUrls = [
-    'https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8',
-    'https://cinehub-test.s3.ap-southeast-3.amazonaws.com/Outputs/hls-hd-en/Avengers-Endgame.m3u8',
-  ];
+  const [videoInfos, setVideoInfos] = useState<VideoInfo[]>([
+    {
+      url: 'https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8',
+      seekTime: 0,
+    },
+    {
+      url: 'https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8',
+      seekTime: 0,
+    },
+  ]);
 
   const switchVideo = () => {
     if (videoRef.current) {
-      const nextVideoIndex = (currentVideoIndex + 1) % videoUrls.length;
+      const nextVideoIndex = (currentVideoIndex + 1) % videoInfos.length;
       setCurrentVideoIndex(nextVideoIndex);
       setVideoLoaded(false);
     }
@@ -25,41 +34,48 @@ const VideoScreen: React.FC = () => {
     if (!videoLoaded) {
       setVideoLoaded(true);
       const currentTime = Math.floor(data.currentTime || 0);
-      setTotalSeekTime(prevTotalSeekTime => prevTotalSeekTime + currentTime);
+      const updatedVideoInfos = [...videoInfos];
+      updatedVideoInfos[currentVideoIndex].seekTime += currentTime;
+      setVideoInfos(updatedVideoInfos);
+      console.log(
+        'time updated' +
+          currentTime +
+          ' ' +
+          updatedVideoInfos[currentVideoIndex].seekTime,
+      );
     }
   };
 
   useEffect(() => {
     if (videoRef.current) {
       if (videoLoaded) {
-        videoRef.current.seek(totalSeekTime);
+        const seekTime = videoInfos[currentVideoIndex].seekTime;
+        videoRef.current.seek(seekTime);
       } else {
         videoRef.current.presentFullscreenPlayer();
       }
     }
-  }, [videoLoaded]);
+  }, [currentVideoIndex, videoInfos, videoLoaded]);
 
   useEffect(() => {
     return () => {
-      setTotalSeekTime(0);
+      // Reset seek times when unmounting the component
+      const resetVideoInfos = videoInfos.map(videoInfo => ({
+        ...videoInfo,
+      }));
+      setVideoInfos(resetVideoInfos);
     };
-  }, []);
+  }, [videoInfos]);
 
   return (
     <View style={{flex: 1}}>
-      <Text
-        style={{
-          fontSize: 16,
-          fontWeight: 'bold',
-          textAlign: 'center',
-          marginTop: 20,
-        }}>
-        Total Seek Time: {totalSeekTime} seconds
-      </Text>
+      {/* <Text style={{ fontSize: 16, fontWeight: "bold", textAlign: "center", marginTop: 20 }}>
+        Total Seek Time: {videoInfos[currentVideoIndex].seekTime} seconds
+      </Text> */}
 
       <Video
         ref={ref => (videoRef.current = ref)}
-        source={{uri: videoUrls[currentVideoIndex]}}
+        source={{uri: videoInfos[currentVideoIndex].url}}
         style={{flex: 1}}
         resizeMode="contain"
         controls={true}
